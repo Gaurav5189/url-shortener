dc2767cbaf0cf4075f0eb1506854057da686b67f
Gaurav5189 <gauravsethi4627@gmail.com>
2026-08-13 22:21:08 +0530

    added Vercel Analytics to the application layout

--- apps/url-shortener-web/app/layout.tsx
+++ apps/url-shortener-web/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Inter, Poppins, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
